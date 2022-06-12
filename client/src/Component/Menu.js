7481ba739bffa78dd4314e7ea950843fc4b0aadf
Michael86868 <michael.kuhnel8@gmail.com>
2022-06-12 21:53:13 +0100

    Modified menu

--- client/src/Component/Menu.js
+++ client/src/Component/Menu.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Menu = () => {
     return (
-        <div>
-            <Link to="/"></Link>
-            <Link to="/add-material"></Link>
-        </div>
+        <Navbar collapseOnSelect expand="lg">
+            <Container>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/" className="fw-bolder">HOME</Nav.Link>
+                        <Nav.Link href="/api" className="fw-bolder">API</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
